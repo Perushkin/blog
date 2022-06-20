103b0ba4f083583822a13de409bad8bac70027c4
Maks <maksim.ushakov02@mail.ru>
2022-06-20 13:18:25 +0300

    deployment

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true,
-// })
-
-module.exports = {
-  publicPath: '/Blog/'
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/blog/'
+    : '/'
+})
